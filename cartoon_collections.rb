64720023549055d8618ab688e924686d161d094b
Maika Kailani <queenmab051@gmail.com>
2019-11-26 19:53:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,7 +37,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(array)
+def find_the_cheese(array0, array1)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
       cheese_types.include?(type)
