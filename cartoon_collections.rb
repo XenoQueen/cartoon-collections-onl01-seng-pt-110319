34301a81e81cbcc326dffeb2bb21fe48f54fd518
Maika Kailani <queenmab051@gmail.com>
2019-11-26 19:41:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(array)
   elements = []
   i = 0
   while i < array.length
-    elements << array[i].capitalize
+    elements << array[i].capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
