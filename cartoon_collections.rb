5e4426753c166c3951149aa365920115df18e2d8
Maika Kailani <queenmab051@gmail.com>
2019-11-26 19:51:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,7 +38,7 @@ def long_planeteer_calls(array)
 end
 
 cheese_types = ["cheddar", "gouda", "camembert"]
-
+snacks = [""]
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
