61cedcc5f9a4369d92b15d7068c4ab0898c8bc78
Maika Kailani <queenmab051@gmail.com>
2019-11-26 19:49:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,9 @@ assorted_words = ["two", "go", "industrious", "bop"]
 def long_planeteer_calls(array)
   i = 0
   if array.any? {|i| i.length > 4}
-    
+    return true
+  else
+    return false
 end
 
 def find_the_cheese# code an argument here
