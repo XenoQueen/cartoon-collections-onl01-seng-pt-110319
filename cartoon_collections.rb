462ea1f33375ef684ccb60591e1e4990c77c71f2
Maika Kailani <queenmab051@gmail.com>
2019-11-26 19:32:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    puts "#{i + 1}.#{array[i]}"
+    i =+ 1
+  end
 end
+
+roll_call_dwarves(dwarves)
 
 def summon_captain_planet# code an argument here
   # Your code here
